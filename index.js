a2e41901bb799216f1335c11451e2ceaba1eb70d
AnikHaque <ekramul15-811@diu.edu.bd>
2023-04-27 22:10:55 +0600

    Api's structured

--- index.js
+++ index.js
@@ -115,14 +115,73 @@ async function run() {
       const result = await productsCollection.find(query).toArray();
       res.send(result);
     });
-   
-
     // GET API for the booked item on my orders route on client side
     app.get("/bookeditems",  async (req, res) => {
       const email = req.query.email;
       const query = { email: email };
       const result = await bookingsCollection.find(query).toArray();
       res.send(result);
+    });
+
+    // GET API to show advertised items
+    app.get("/advertised", async (req, res) => {
+      const query = {};
+      const result = await postedProductsCollection.find(query).toArray();
+      res.send(result);
+    });
+    app.get("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const booking = await bookingsCollection.findOne(query);
+      res.send(booking);
+    });
+
+    // api to get users data
+    app.get("/users", async (req, res) => {
+      const query = {};
+      const result = await usersCollection.findOne(query);
+      res.send(result);
+    });
+
+    // admin to see all sellers and all nuyers route
+    app.get("/users/admin/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isAdmin: user?.role === "admin" });
+    });
+
+    // api to check sellers to access add a products and my products
+    app.get("/users/seller/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isSeller: user?.role === "Seller" });
+    });
+
+    // api to check buyers to access my orders on dashboard
+    app.get("/users/buyers/:email", async (req, res) => {
+      const email = req.params.email;
+      const query = { email: email };
+      const user = await usersCollection.findOne(query);
+      res.send({ isBuyer: user?.role === "Buyer" });
+    });
+
+    // show all sellers users on all sellers route
+    app.get("/users/allsellers", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      const allsellers = users.filter((user) => user?.role === "Seller");
+      res.send(allsellers);
+    });
+
+    // api to show all buyers users on all buyers route
+    app.get("/users/allbuyers", async (req, res) => {
+      const query = {};
+      const users = await usersCollection.find(query).toArray();
+      const allbuyers = users.filter((user) => user?.role === "Buyer");
+      res.send(allbuyers);
     });
 
 // -------------All GET API's End----------------
@@ -166,6 +225,26 @@ async function run() {
       const result = await itemCollection.insertOne(posteditem);
       res.send(result);
     });
+    
+  
+  // api to post advertised items
+  app.post("/advertisedproducts", async (req, res) => {
+    const advertisedProducts = req.body;
+    const advertised = await postedProductsCollection.insertOne(
+      advertisedProducts
+    );
+    res.send(advertised);
+  });
+
+  // api to create registered user data in db
+  app.post("/users", async (req, res) => {
+    const user = req.body;
+    const result = await usersCollection.insertOne(user);
+    res.send(result);
+  });
+  
+  
+
     // -------------All POST API's end----------------
 
     // -------------All DELETE API's Start----------------
@@ -212,12 +291,27 @@ async function run() {
       res.send(result);
     });
 // -------------All UPDATE API's end----------------
-    app.get("/bookings/:id", async (req, res) => {
-      const id = req.params.id;
-      const query = { _id: ObjectId(id) };
-      const booking = await bookingsCollection.findOne(query);
-      res.send(booking);
+ 
+
+    // ------------Payment API integrated--------------
+
+    // api to store payment info on db
+    app.post("/payments", async (req, res) => {
+      const payment = req.body;
+      const result = await paymentsCollection.insertOne(payment);
+      const id = payment.bookingId;
+      const filter = { _id: ObjectId(id) };
+      const updatedDoc = {
+        $set: {
+          paid: true,
+          trandactionId: payment.transactionId,
+        },
+      };
+      const updated = await bookingsCollection.updateOne(filter, updatedDoc);
+
+      res.send(result);
     });
+
     // payment method implementaion
     app.post("/create-payment-intent", async (req, res) => {
       const booking = req.body;
@@ -233,95 +327,7 @@ async function run() {
         clientSecret: paymentIntent.client_secret,
       });
 
-      // api to store payment info on db
-      app.post("/payments", async (req, res) => {
-        const payment = req.body;
-        const result = await paymentsCollection.insertOne(payment);
-        const id = payment.bookingId;
-        const filter = { _id: ObjectId(id) };
-        const updatedDoc = {
-          $set: {
-            paid: true,
-            trandactionId: payment.transactionId,
-          },
-        };
-        const updated = await bookingsCollection.updateOne(filter, updatedDoc);
-
-        res.send(result);
-      });
     });
-
-    
-    // api to post advertised items
-    app.post("/advertisedproducts", async (req, res) => {
-      const advertisedProducts = req.body;
-      const advertised = await postedProductsCollection.insertOne(
-        advertisedProducts
-      );
-      res.send(advertised);
-    });
-
-    // api to show advertised items
-    app.get("/advertised", async (req, res) => {
-      const query = {};
-      const result = await postedProductsCollection.find(query).toArray();
-      res.send(result);
-    });
-         
-              // api to create registered user data in db
-              app.post("/users", async (req, res) => {
-                const user = req.body;
-                const result = await usersCollection.insertOne(user);
-                res.send(result);
-              });
-          
-              // api to get users data
-              app.get("/users", async (req, res) => {
-                const query = {};
-                const result = await usersCollection.findOne(query);
-                res.send(result);
-              });
-          
-              // admin to see all sellers and all nuyers route
-              app.get("/users/admin/:email", async (req, res) => {
-                const email = req.params.email;
-          
-                const query = { email: email };
-                const user = await usersCollection.findOne(query);
-                res.send({ isAdmin: user?.role === "admin" });
-              });
-          
-              // api to check sellers to access add a products and my products
-              app.get("/users/seller/:email", async (req, res) => {
-                const email = req.params.email;
-                const query = { email: email };
-                const user = await usersCollection.findOne(query);
-                res.send({ isSeller: user?.role === "Seller" });
-              });
-          
-              // api to check buyers to access my orders on dashboard
-              app.get("/users/buyers/:email", async (req, res) => {
-                const email = req.params.email;
-                const query = { email: email };
-                const user = await usersCollection.findOne(query);
-                res.send({ isBuyer: user?.role === "Buyer" });
-              });
-          
-              // show all sellers users on all sellers route
-              app.get("/users/allsellers", async (req, res) => {
-                const query = {};
-                const users = await usersCollection.find(query).toArray();
-                const allsellers = users.filter((user) => user?.role === "Seller");
-                res.send(allsellers);
-              });
-          
-              // api to show all buyers users on all buyers route
-              app.get("/users/allbuyers", async (req, res) => {
-                const query = {};
-                const users = await usersCollection.find(query).toArray();
-                const allbuyers = users.filter((user) => user?.role === "Buyer");
-                res.send(allbuyers);
-              });
     } 
     finally {
       
