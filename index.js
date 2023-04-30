d46a005faadf4757bb116be8d7b8b547c3d92db7
AnikHaque <ekramul15-811@diu.edu.bd>
2023-04-30 13:34:09 +0600

    added env

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(cors());
 app.use(express.json()); 
 
 // mongodb uri connection 
-const uri = `mongodb+srv://partsix:0iSTi7C1EL0AWIJJ@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lx750.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 // jwt function 
